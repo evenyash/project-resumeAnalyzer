96da4fbc5a0e40323873d24d1cec718f8b029ed4
yash <sinhayash851@gmail.com>
2026-03-13 10:18:25 +0530

    cookie error fixing

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -55,6 +55,7 @@ export const registerUser = async (req, res) => {
       id: user._id,
       username: user.username,
       email: user.email,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     },
   });
 };
@@ -100,6 +101,7 @@ export const loginUser = async (req, res) => {
     httpOnly: true,
     secure: true,
     sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
   res.status(200).json({
