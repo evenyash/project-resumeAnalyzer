12225aea71c6aabc8736d2453b2e131cb038483a
yash <sinhayash851@gmail.com>
2026-03-13 09:42:18 +0530

    fixed cookie error

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -43,7 +43,11 @@ export const registerUser = async (req, res) => {
     { expiresIn: "1d" },
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
 
   res.status(201).json({
     message: "User registered successfully",
@@ -92,7 +96,11 @@ export const loginUser = async (req, res) => {
     { expiresIn: "1d" },
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
 
   res.status(200).json({
     message: "User logged in successfully",
